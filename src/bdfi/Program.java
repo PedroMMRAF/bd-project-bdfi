481c49588955be92369ad40c60b6c7edb2bd11e6
Pedro Fernandes <Pedro Fernandes@DESKTOP-DMFNVD7>
2021-11-10 16:34:06 +0000

    update
    Change-Id: I6d3f32194510a8bcb43537cc1b68285c2d9972dd

--- src/bdfi/Program.java
+++ src/bdfi/Program.java
@@ -1,5 +1,33 @@
 package bdfi;
 
 public interface Program {
+
+	/**
+	 * 
+	 * @return
+	 */
+	String getId();
+
+	/**
+	 * 
+	 * @return
+	 */
+	String getTitle();
+
+	/**
+	 * 
+	 * @return
+	 */
+	int getProductionYear();
 	
+	/**
+	 * @return If the 
+	 */
+	boolean isProducing();
+	
+	/**
+	 * Finishes production of this program
+	 */
+	void finishProduction();
+
 }
--- src/bdfi/ProgramClass.java
+++ src/bdfi/ProgramClass.java
@@ -1,25 +1,42 @@
 package bdfi;
 
+import java.time.LocalDateTime;
+
 public class ProgramClass implements Program {
 	protected String id;
 	protected String title;
 	protected int prodYear;
+	protected boolean producing;
 
 	public ProgramClass(String id, String title, int prodYear) {
 		this.id = id;
 		this.title = title;
 		this.prodYear = prodYear;
+		this.producing = prodYear == LocalDateTime.now().getYear();
 	}
-
+	
+	@Override
 	public String getId() {
 		return id;
 	}
-
+	
+	@Override
 	public String getTitle() {
 		return title;
 	}
-
-	public int getProdYear() {
+	
+	@Override
+	public int getProductionYear() {
 		return prodYear;
+	}
+	
+	@Override
+	public boolean isProducing() {
+		return producing;
+	}
+	
+	@Override
+	public void finishProduction() {
+		producing = false;
 	}
 }
