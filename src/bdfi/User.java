b9bc5feaeebb3f024b528a6d19fe5da4b4f3c748
GuiSantana08 <gf.santana@campus.fct.unl.pt>
2021-11-10 16:16:04 +0000

    User
    Change-Id: Ifa27960cc3734d5b46bb5baee15724799a2ba68c

--- src/bdfi/User.java
+++ src/bdfi/User.java
@@ -2,5 +2,16 @@ package bdfi;
 
 public interface User {
 	
+	String getID();
+	
+	String getName();
+	
+	int getBirthYear();
+	
+	String getEmail();
+	
+	String getPhoneNumber();
+	
+	String getGender();
 
 }
